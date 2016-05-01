feb17a03e552386c2349376b4bbd2dae903a8253
Bruno Dupuis <bruno.dupuis@novapost.fr>
2016-05-01 13:51:02 +0200

    added comments in example grammar

--- examples/calculator.py
+++ examples/calculator.py
@@ -3,15 +3,35 @@ from fastidious import Parser
 
 
 class Calculator(Parser):
+
+    # __grammar__ is the PEG definition. Each `rulename <- a rule`
+    # adds a method `Calculator.rulename()`. This methods tries to
+    # match the input at current position
     __grammar__ = r"""
+    # the label is ommited. `:expr` is equivalent to `expr:expr`
     eval <- :expr EOF {@expr}
-    expr <- _ first:term rest:( _ add_op _ term  )* _
+
+    # action {on_expr} calls `Calculator.on_expr(self, value, first, rest)`
+    # on match. `first` and `rest` args are the labeled parts of the rule
     term <- first:factor rest:( _ mult_op _ factor )* {on_expr}
+
+    # Because the Parser has a method named `on_expr` ("on_" + rulename)
+    # this method is the implicit action of this rule. We omitted {on_expr}
+    expr <- _ first:term rest:( _ add_op _ term  )* _
+
+    # there's no explicit or implicit action. These rules return their exact
+    # matches
     add_op <- '+' / '-'
     mult_op <- '*' / '/'
-    factor <- ( '(' factor:expr ')' ) / factor:integer {@factor}
+
+    # action {@fact} means : return only the match of part labeled `fact`.
+    factor <- ( '(' fact:expr ')' ) / fact:integer {@fact}
+
     integer <- '-'? [0-9]+
     _ <- [ \n\t\r]*
+
+    # this one is tricky. `.` means "any char". At EOF there's no char,
+    # thus Not any char, thus `!.`
     EOF <- !.
     """
 
