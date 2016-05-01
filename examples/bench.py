5cb789ce6c4a6a426cfe6b32c7fcc4ed519bae9d
Bruno Dupuis <bruno.dupuis@novapost.fr>
2016-05-01 13:49:32 +0200

    Refactor benchmarks (inspired by parsimonious benchmarks)

--- examples/bench.py
+++ examples/bench.py
@@ -1,36 +1,119 @@
-import timeit
+import gc
+from timeit import repeat
 
 from fastidious.parser import _GrammarParser
+from fastidious.bootstrap import _GrammarParserBootstraper
 from fastidious import Parser
 
 grammar = '\n'.join(
     [l.strip() for l in _GrammarParser.__grammar__.splitlines()])
 
+kb = len(grammar) / 1024.0
+
+class Default(Parser):
+    __grammar__ = grammar
+
+
 class NotMemoized(Parser):
     __memoize__ = False
     __grammar__ = grammar
 
-bootstrap_stmt = "_GrammarParserBootstraper(%s).grammar()" % repr(grammar)
-bs_nb = 50
 
-time = timeit.timeit(
-    bootstrap_stmt,
-    setup="from fastidious.bootstrap import _GrammarParserBootstraper",
-    number=bs_nb)
-print"bootstrap\t\t", time, "s   ", time/bs_nb, "s/op   ", bs_nb/time, "op/s"
+class NotJSONParser(Parser):
+    # __debug___ r True
+    __inline__ = True
+    __grammar__ = r"""
+        value <- space (string / number / object / array / true_false_null)
+                space
 
-generated_stmt = "_GrammarParser(%s).grammar()" % repr(grammar)
-gnr_nb = 50
-time = timeit.timeit(
-    generated_stmt,
-    setup="from fastidious.parser import _GrammarParser",
-    number=gnr_nb)
-print"generated\t\t", time, "s   ", time/gnr_nb, "s/op   ", gnr_nb/time, "op/s"
+        object <- "{" members "}"
+        members <- (pair ("," pair)*)?
+        pair <- string ":" value
+        array <- "[" elements "]"
+        elements <- (value ("," value)*)?
+        true_false_null <- "true" / "false" / "null"
 
-nm_stmt = "NotMemoized(%s).grammar()" % repr(grammar)
-nm_nb = 50
-time = timeit.timeit(
-    nm_stmt,
-    setup="from __main__ import NotMemoized",
-    number=nm_nb)
-print"not memoized\t\t", time, "s   ", time/nm_nb, "s/op   ", nm_nb/time, "op/s"
+        string <- space "\"" chars "\"" space
+        #chars <- ( !'"' . )* {flatten}
+        chars <- ~"[^\"]*"  # TODO implement the real thing
+        number <- (int frac exp) / (int exp) / (int frac) / int
+        int <- "-"? ((digit1to9 digits) / digit)
+        frac <- "." digits
+        exp <- e digits
+        digits <- digit+
+        e <- "e+" / "e-" / "e" / "E+" / "E-" / "E"
+
+        # digit1to9 <- [1-9]
+        # digit <- [0-9]
+        # space <- [ \t\r\n]*
+        digit1to9 <- ~"[1-9]"
+        digit <- ~"[0-9]"
+        space <- ~"\\s*"
+
+    """
+
+
+father = """{
+    "id" : 1,
+    "married" : true,
+    "name" : "Larry Lopez",
+    "sons" : null,
+    "daughters" : [
+        {
+        "age" : 26,
+        "name" : "Sandra"
+        },
+        {
+        "age" : 25,
+        "name" : "Margaret"
+        },
+        {
+        "age" : 6,
+        "name" : "Mary"
+        }
+        ]
+    }"""
+more_fathers = ','.join([father] * 60)
+json = '{"fathers" : [' + more_fathers + ']}'
+
+
+def benchit(klass, source, entry_point, ref=None):
+    entry_point = getattr(klass, entry_point)
+    # test the parser correctness
+    p = klass(source)
+    entry_point(p)
+    assert p.p_suffix() == ""
+
+    gc.collect()
+
+    NUMBER = 1
+    REPEAT = 5
+    total_seconds = min(repeat(lambda: entry_point(klass(source)),
+                               lambda: gc.enable(),
+                               repeat=REPEAT,
+                               number=NUMBER))
+    seconds_each = total_seconds / NUMBER
+    t = seconds_each
+
+    if ref is not None:
+        if isinstance(ref, float):
+            var = "(%.1f%%)" % ((ref-t)*100/ref)
+        else:
+            var = ref
+    else:
+        var = ""
+
+    kb = len(source) / 1024.0
+    print '%-25s: Took %.3fs to parse %.1fKB: %.0fKB/s %s' % (
+        klass.__name__, seconds_each, kb, kb / seconds_each, var)
+    return seconds_each
+
+benchit(NotJSONParser, json, "value")
+ref = benchit(_GrammarParser, grammar, "grammar", "(base)")
+benchit(_GrammarParserBootstraper, grammar, "grammar", ref)
+ref = benchit(Default, grammar, "grammar", "(base)")
+benchit(NotMemoized, grammar, "grammar", ref)
+
+default = Default(grammar).grammar()
+nm = NotMemoized(grammar).grammar()
+assert default == nm
