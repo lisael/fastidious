e310eb44e403eecf8a665ca388350c63d9db791f
Bruno Dupuis <bruno.dupuis@novapost.fr>
2016-05-29 15:04:53 +0200

    Convert README to .rst and added setup.py

---
+++ setup.py
@@ -0,0 +1,68 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='fastidious',
+
+    version='0.1dev0',
+
+    description='Yet another python parser generator',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/lisael/fastidious',
+
+    # Author details
+    author='lisael',
+    author_email='lisael@lisael.org',
+
+    license='GPLv3',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Software Development :: Compilers',
+
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='PEG parser generator lexer',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['fastidious', 'examples', 'tests']),
+
+    install_requires=[],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    #extras_require={
+        #'dev': ['check-manifest'],
+        #'test': ['coverage'],
+    #},
+
+)
+
